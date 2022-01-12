b7818634993a669542b3262c34c469dfab831adc
JScrivo <scrivjor@sheridancollege.ca>
2022-01-12 15:31:41 -0500

    Initial Implementation of CRUD for Inventory, Location, and Price Tables

---
+++ ShopifyDemoProject/Controllers/InventoryController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopifyDemoProject.Models;
+
+namespace ShopifyDemoProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public InventoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var inventories = _db.Inventories.ToList();
+            return new JsonResult(inventories);
+        }
+
+        /*[HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var inventory = await _db.Inventories.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null) return BadRequest("Inventory not found");
+            return new JsonResult(product);
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Inventory inventory)
+        {
+            var existing = await _db.Inventories.FirstOrDefaultAsync(x => x.ItemID.Equals(inventory.ItemID) && x.LocationID.Equals(inventory.LocationID));
+            if (existing != null) return BadRequest("Inventory already exists for this item at this location.");
+
+            _db.Inventories.Add(inventory);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult(new { ItemID = inventory.ItemID, LocationID = inventory.LocationID });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Inventory inventory)
+        {
+            var existing = await _db.Inventories.FirstOrDefaultAsync(x => x.ItemID.Equals(inventory.ItemID) && x.LocationID.Equals(inventory.LocationID));
+            if (existing == null) return BadRequest("Inventory not found");
+
+            existing.Quantity = inventory.Quantity;
+
+            _db.Inventories.Update(existing);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult(new { ItemID = inventory.ItemID, LocationID = inventory.LocationID });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int itemID, int locationID)
+        {
+            var inventory = await _db.Inventories.FirstOrDefaultAsync(x => x.ItemID.Equals(itemID) && x.LocationID.Equals(locationID));
+            if (inventory == null) return BadRequest("Inventory not found");
+
+            _db.Inventories.Remove(inventory);
+            await _db.SaveChangesAsync();
+
+            return new OkObjectResult("Inventory deleted");
+        }
+
+        public float CheckRemainingInventory(int locationID)
+        {
+            var inventory =  _db.Inventories.Where(x => x.LocationID == locationID);
+            float capacity = _db.Locations.First(x => x.Id == locationID).Capacity;
+
+            foreach (Inventory item in inventory)
+            {
+                Product product = _db.Products.First(x => x.Id == item.ItemID);
+                capacity -= product.VolPerUnit * item.Quantity;
+            }
+
+            return capacity;
+        }
+    }
+}
---
+++ ShopifyDemoProject/Controllers/PriceController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopifyDemoProject.Models;
+
+namespace ShopifyDemoProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PriceController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public PriceController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var prices = _db.Prices.ToList();
+            return new JsonResult(prices);
+        }
+
+        /*[HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var location = await _db.Locations.FirstOrDefaultAsync(x => x.Id == id);
+            if (location == null) return BadRequest("Location not found");
+            return new JsonResult(location);
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Price price)
+        {
+            var existing = await _db.Prices.FirstOrDefaultAsync(x => x.ItemID.Equals(price.ItemID) && x.LocationID.Equals(price.LocationID));
+            if (existing != null) return BadRequest("Price for this item has already been set for this location.");
+
+            _db.Prices.Add(price);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult(new { ItemID = price.ItemID, LocationID = price.LocationID });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Price price)
+        {
+            var existing = await _db.Prices.FirstOrDefaultAsync(x => x.ItemID.Equals(price.ItemID) && x.LocationID.Equals(price.LocationID));
+            if (existing == null) return BadRequest("Price for this item does not exist at this location.");
+
+            existing.UnitPrice = price.UnitPrice;
+
+            _db.Prices.Update(existing);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult(new { ItemID = price.ItemID, LocationID = price.LocationID });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int itemID, int locationID)
+        {
+            var existing = await _db.Prices.FirstOrDefaultAsync(x => x.ItemID.Equals(itemID) && x.LocationID.Equals(locationID));
+            if (existing == null) return BadRequest("Price for this item not found at specified location.");
+
+            _db.Prices.Remove(existing);
+            await _db.SaveChangesAsync();
+
+            return new OkObjectResult("Pricing information deleted");
+        }
+    }
+}
